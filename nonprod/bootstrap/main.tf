39a29056162d4ccf2e94fa74cb24d7a4e35a7266
Ranka Surender <vrankasurender@deloitte.com>
2025-11-11 10:47:30 +0530

    Added tfvars sample files

--- nonprod/bootstrap/main.tf
+++ nonprod/bootstrap/main.tf
@@ -74,6 +74,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
     id     = "expire-old-versions"
     status = "Enabled"
 
+    filter {}
+
     noncurrent_version_expiration {
       noncurrent_days = 90
     }

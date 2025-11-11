753a5683a58770a5c7d753a5e6473eefd232cefc
Ranka Surender <vrankasurender@deloitte.com>
2025-11-11 23:02:57 +0530

    Added LB Ingress Files

--- nonprod/eks/dev/argocd.tf
+++ nonprod/eks/dev/argocd.tf
@@ -1,52 +1,7 @@
 ################################################################################
 # ArgoCD Installation for Dev Cluster
 ################################################################################
-
-################################################################################
-# Helm Provider Configuration
-################################################################################
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args = [
-        "eks",
-        "get-token",
-        "--cluster-name",
-        module.eks.cluster_name,
-        "--region",
-        var.aws_region
-      ]
-    }
-  }
-}
-
-################################################################################
-# Kubernetes Provider Configuration
-################################################################################
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      module.eks.cluster_name,
-      "--region",
-      var.aws_region
-    ]
-  }
-}
+# Note: Kubernetes and Helm providers are configured in providers.tf
 
 ################################################################################
 # ArgoCD Module

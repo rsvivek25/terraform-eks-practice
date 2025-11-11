43aea42a340f7c9bb1cf085642fcaa9c52f6c4e5
Ranka Surender <vrankasurender@deloitte.com>
2025-11-11 23:11:05 +0530

    Added LB Ingress Files

---
+++ modules/alb-ingress-class/versions.tf
@@ -0,0 +1,14 @@
+################################################################################
+# ALB Ingress Class Module - Terraform Configuration
+################################################################################
+
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.1"
+    }
+  }
+}
